8d354add58c1f8b9c2dca1fb89f50d01a10f1dd3
Alexander <aleasims@gmail.com>
2018-09-14 15:05:42 +0300

    Path fixed: now bicycle is head directory

--- core/run/db_manager_start.py
+++ core/run/db_manager_start.py
@@ -1,9 +1,9 @@
 import sys
 import os
-sys.path.insert(0, os.path.join(os.environ['BC_DIR'], 'core'))
+sys.path.insert(0, os.environ['BC_DIR'])
 
-from database.db_manager import DBManager
-from common import configure_logger, load_config
+from core.database.db_manager import DBManager
+from core.common import configure_logger, load_config
 
 
 def main():
--- core/run/web_server_start.py
+++ core/run/web_server_start.py
@@ -1,9 +1,9 @@
 import sys
 import os
-sys.path.insert(0, os.path.join(os.environ['BC_DIR'], 'core'))
+sys.path.insert(0, os.environ['BC_DIR'])
 
-from web.web_server import WebServer
-from common import configure_logger, load_config
+from core.web.web_server import WebServer
+from core.common import configure_logger, load_config
 
 
 def main():

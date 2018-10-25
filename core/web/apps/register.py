10de3352c4aad11cbc658dca5ef290055f17f1a9
Alexander <aleasims@gmail.com>
2018-10-25 16:49:24 +0300

    [#6] Data proceeding to DB

--- core/web/apps/register.py
+++ core/web/apps/register.py
@@ -1,8 +1,9 @@
 from urllib import parse
+from core.database.db_client import DBClient
 
 
 def activate(args):
-    response = b'HTTP/1.1 204\r\n'
+    response = b'HTTP/1.1 406\r\n'
     try:
         query = parse.parse_qs(args['input'].decode('utf-8'))
     except KeyError:
@@ -10,5 +11,9 @@ def activate(args):
 
     name = query['name'][0]
     passwd = query['passwd_hash'][0]
+
+    cli = DBClient('/tmp/bc_ipc')
+    if cli.create_new_user(name, passwd):
+        response = b'HTTP/1.1 204\r\n'
 
     return response

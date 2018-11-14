c118b05a81899955bf0a0bafb21ffbdf27757adc
Alexander <aleasims@gmail.com>
2018-11-14 12:30:30 +0300

    [#9] Server upgaded to threading (roughly)

--- core/web/server.py
+++ core/web/server.py
@@ -1,20 +1,25 @@
+from socketserver import ThreadingMixIn
 from http.server import HTTPServer
 from core.web.handler import WebHandler
+
+
+class ServerClass(ThreadingMixIn, HTTPServer):
+    pass
 
 
 class WebServer:
     def __init__(self, config, logger):
         self.logger = logger
         self.config = config
+        self.HOST, self.PORT = config['host'], config['port']
+        WebHandler.logger = self.logger
+        self.server = ServerClass((self.HOST, self.PORT), WebHandler)
+        self.server.allow_reuse_address = True
+        self.server.version = config['version']
+        self.server.www_dir = config['www_dir']
 
     def start(self):
-        HOST, PORT = self.config['host'], self.config['port']
-        HTTPServer.allow_reuse_address = True
-        HTTPServer.version = 'BicycleWEB/0.1b'
-        HTTPServer.www_dir = '/home/alea/bicycle/www'
-        self.logger.info('Starting server on {}:{}'.format(HOST, PORT))
-        WebHandler.logger = self.logger
-        self.server = HTTPServer((HOST, PORT), WebHandler)
+        self.logger.info('Starting server on {}:{}'.format(self.server.server_name, self.server.server_port))
         try:
             self.server.serve_forever()
         except KeyboardInterrupt:

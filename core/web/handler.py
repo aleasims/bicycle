c2b0717eb8e85cdb4ff4d48dc6247daa3245c21e
Alexander <aleasims@gmail.com>
2018-11-21 12:08:24 +0300

    [#9] small fix

--- core/web/handler.py
+++ core/web/handler.py
@@ -114,6 +114,8 @@ class WebHandler(SimpleHTTPRequestHandler):
         app = None
         try:
             app = importlib.import_module('core.web.apps.' + args['app'])
+            if not hasattr(app, 'activate'):
+                raise ImportError
         except ImportError:
             self.send_error(HTTPStatus.NOT_IMPLEMENTED)
             return

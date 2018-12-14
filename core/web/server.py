e67fa4a1096fc19f5313789b21a070c70a724a9a
sn0wbars <alex-borz@yandex.ru>
2018-12-14 22:11:13 +0300

    implement TLS

--- core/web/server.py
+++ core/web/server.py
@@ -1,10 +1,15 @@
+import ssl
 from socketserver import ThreadingMixIn
 from http.server import HTTPServer
 from core.web.handler import WebHandler
 
 
 class ServerClass(ThreadingMixIn, HTTPServer):
-    pass
+    def get_request(self):
+        sock, addr = self.socket.accept()
+        if self.use_tls:
+            sock = self.context.wrap_socket(sock, server_side=True)
+        return sock, addr
 
 
 class WebServer:
@@ -17,6 +22,11 @@ class WebServer:
         self.server.allow_reuse_address = True
         self.server.version = config['version']
         self.server.www_dir = config['www_dir']
+
+        self.server.use_tls = config['use_tls']
+        if config['use_tls']:
+            self.server.context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+            self.server.context.load_cert_chain(config['certificate'], keyfile=config['keyfile'])
 
     def start(self):
         self.logger.info('Starting server on {}:{}'.format(self.server.server_name, self.server.server_port))
